98dba6277783f25e34c6b2c08066e2d149f192a6
+ <familymanjosh@gmail.com>
2022-12-16 20:35:16 -0800

    commit tree dbd617e39887be3a9a5e70ddf6a29275c55f1404

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
+let num1 = 31
+let num2 = 2
+let multiply = num1*num2;
+let random = Math.floor(Math.random() * 10) + 1;
+let num3 = 3.5
+let num4 = Math.ceil(num3)
+let mod = num4 ;
+let max = Math.max(5, 10, 15, 20);
 // Write your code here
